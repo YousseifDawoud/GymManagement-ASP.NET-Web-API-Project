831d805306c1c63d2fef22b1131a645ea92b83aa
YousseifDawoud <yousseifdawoud141@gmail.com>
2025-12-18 13:02:03 +0200

    Add BaseEntity Class

---
+++ Gym.Domain/Common/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Gym.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public int Id { get; protected set; }
+    public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; protected set; }
+
+    protected void SetUpdatedAt() => UpdatedAt = DateTime.UtcNow;
+}
+
+// Why The Class Is Abstract ? 
+// What Is the Meanig The Set Accessor Is Protected ?
+// Why I`m Choise DateTime ?
+// What Defference Between DateTime.Now And DateTime.UtcNow ?
