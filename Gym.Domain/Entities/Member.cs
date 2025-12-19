2db9e89fcbcd594b1656a79b3d9be3597d5d1bf0
YousseifDawoud <yousseifdawoud141@gmail.com>
2025-12-19 16:53:31 +0200

    Update Some Properies

--- Gym.Domain/Entities/Member.cs
+++ Gym.Domain/Entities/Member.cs
@@ -6,38 +6,38 @@ using System.Text;
 
 namespace Gym.Domain.Entities;
 
-    public class Member : BaseEntity
-    {
-        public string FullName { get; private set; } = null!;
-        public string Phone { get; private set; } = null!;
-        public string Email { get; private set; } = null!;
+public class Member : BaseEntity
+{
+    public string FullName { get; private set; } = null!;
+    public string Phone { get; private set; } = null!;
+    public string Email { get; private set; } = null!;
 
-        public DateOnly MembershipStartDate { get; private set; }
-        public DateOnly MembershipEndDate { get; private set; }
-        public MembershipStatus Status { get; private set; }
-        public int MembershipPlanId { get; private set; }
-        public MembershipPlan MembershipPlan { get; private set; } = null!;
+    public DateOnly MembershipStartDate { get; private set; }
+    public DateOnly MembershipEndDate { get; private set; }
+    public MembershipStatus Status { get; private set; }
+    public int MembershipPlanId { get; private set; }
+    public MembershipPlan MembershipPlan { get; private set; } = null!; // Navigation property
 
-        private readonly List<Booking> _bookings = new();
-        public IReadOnlyCollection<Booking> Bookings => _bookings;
+    private readonly List<Booking> _bookings = new();
+    public IReadOnlyCollection<Booking> Bookings => _bookings;
 
-        private Member() { }
+    private Member() { }
 
-        public Member(
-            string fullName,
-            string phone,
-            string email,
-            DateOnly startDate,
-            DateOnly endDate,
-            int membershipPlanId
-            )
-           => (FullName, Phone, Email, MembershipStartDate, MembershipEndDate, MembershipPlanId, Status)
-            = (fullName, phone, email, startDate, endDate, membershipPlanId, MembershipStatus.Active);
+    public Member(
+        string fullName,
+        string phone,
+        string email,
+        DateOnly startDate,
+        DateOnly endDate,
+        int membershipPlanId
+        )
+        => (FullName, Phone, Email, MembershipStartDate, MembershipEndDate, MembershipPlanId, Status)
+        = (fullName, phone, email, startDate, endDate, membershipPlanId, MembershipStatus.Active);
 
-         public void ExpireMembership()
-         {
+        public void ExpireMembership()
+        {
             Status = MembershipStatus.Expired;
             SetUpdated();
-         }
-    }
+        }
+}
 
--- Gym.Domain/Entities/Trainer.cs
+++ Gym.Domain/Entities/Trainer.cs
@@ -3,19 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Gym.Domain.Entities
+namespace Gym.Domain.Entities;
+
+public class Trainer : BaseEntity
 {
-    public class Trainer : BaseEntity
-    {
-        public string FullName { get; set; } = null!;
-        public string Specialty { get; set; } = null!;
+    public string FullName { get; private set; } = null!;
+    public string Specialty { get; private set; } = null!;
 
-        private readonly List<Session> _sessions = new();
-        public IReadOnlyCollection<Session> Sessions => _sessions;
+    private readonly List<Session> _sessions = new();
+    public IReadOnlyCollection<Session> Sessions => _sessions;
 
-        private Trainer() { }
+    private Trainer() { }
 
-        public Trainer(string fullName, string specialty)
-        => (FullName , Specialty) = (fullName, specialty);
-    }
+    public Trainer(string fullName, string specialty)
+    => (FullName, Specialty) = (fullName, specialty);
 }
+
+// You Should Know Why You Make The Set Accessor Is Private 
+// the new Syntax for initializing new list like list<Session>
+// What Is the Defference Between IReadOnlyCollection and IEnumerable
+// Using expression-bodied members for constructor
