46f559f4dc397e60ee59a2ab32431413dbcf47df
YousseifDawoud <yousseifdawoud141@gmail.com>
2025-12-19 14:06:30 +0200

    Add MembershipPlan Entity

---
+++ Gym.Domain/Entities/MembershipPlan.cs
@@ -0,0 +1,16 @@
+﻿using Gym.Domain.Common;
+
+namespace Gym.Domain.Entities
+{
+    public class MembershipPlan : BaseEntity
+    {
+        public string Name { get; protected set; } = null!;
+        public decimal Price { get; protected set; }
+        public int MaxSessionsPerMonth { get; set; }
+        private MembershipPlan() { } // For EF Core
+
+
+        public MembershipPlan(string name, decimal price, int maxSessionsPerMonth)
+        => (Name, Price, MaxSessionsPerMonth) = (name, price, maxSessionsPerMonth);
+    }
+}
